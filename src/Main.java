d9987a6f5141e830859ea871575dff06c6296500
FelipeOthon <felipeosantiago@gmail.com>
2022-10-15 14:38:29 -0300

    enviar exercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int a, b, c, d, res1, res2, dif;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nFazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto \n" +
+                "de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).\n \n ");
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scanner.nextInt();
+
+        System.out.println("Digite o segundo valor: ");
+        b = scanner.nextInt();
+
+        System.out.println("Digite o terceiro valor: ");
+        c = scanner.nextInt();
+
+        System.out.println("Digite o quarto valor: ");
+        d = scanner.nextInt();
+        res1 = a + b;
+        res2 = c + b;
+        dif = res1 - res2;
+
+        System.out.printf("A diferença %d e %d é igual a : %d", res1, res2, dif);
+
+        scanner.close();
     }
 }
